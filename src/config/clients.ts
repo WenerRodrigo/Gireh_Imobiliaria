0c53b2f2343118711a42b948f901fbeb0707c4e0
Wener Rodrigo <wener19.wr@gmail.com>
2025-09-03 09:44:55 -0300

    chore: initial white label setup

---
+++ src/config/clients.ts
@@ -0,0 +1,20 @@
+export const clients = {
+    shadai: {
+        name: "Shadai Imobiliária",
+        logo: "/src/assets/logo.png",
+        colors: {
+            primary: "",
+            secondary: ""
+        }
+    },
+    contact: {
+
+    },
+    legal: {
+
+    },
+    seo: {
+        title: "",
+        description: ""
+    }
+}
